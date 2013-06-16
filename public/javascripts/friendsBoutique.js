87deb9bde53277ac07368c215b6389e9ea1c8799
gorerider <wk@plan-of-whistler.de>
2013-06-16 20:56:38 +0200

    added bootstrap + layout

---
+++ public/javascripts/friendsBoutique.js
@@ -0,0 +1,129 @@
+(function(d, c){
+
+	// functions
+	var getMeta = function (property, propertyValue, value) {	
+		var metaTags = d.getElementsByTagName("meta");
+		var result;
+		
+		for (var i = 0; i < metaTags.length; i++) {
+			var p = metaTags[i].getAttribute(property);
+			if (p == propertyValue) {
+				result = metaTags[i].getAttribute(value);
+			}
+		}
+		
+		return result;
+	}
+	
+	// ajax method
+	var ajaxRequest = function (url, method, data, callback) {
+		var xhttp = false;
+		
+		// ff, safari
+		if(window.XMLHttpRequest) {
+			xhttp = new XMLHttpRequest();
+		// ie
+		} else if (window.ActiveXObject) {
+			xhttp = new ActiveXObject();
+		}
+		
+		if(xhttp != false) {
+			// Open Http Request connection
+			xmlHttpRequst.open(method, url, true);
+			// Set request header (optional if GET method is used)
+			xmlHttpRequst.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+			// Callback when ReadyState is changed.
+			xmlHttpRequst.onreadystatechange = function()
+			{
+				if (xmlHttpRequst.readyState == 4)
+				{
+					if(typeof callback == "function") {
+						callback();
+					}
+					
+					return xmlHttpRequst.responseText;
+				}
+			}
+			xmlHttpRequst.send(data);
+		}
+	}
+
+	// get div container
+	var container = d.getElementById(c);
+	container.setAttribute("style", "display: inline-block;");
+	
+	if(!container) return;
+	
+	// define some vars
+	var link, icon, iframediv, iframe;
+	
+	// link
+	link = d.createElement("a");
+	link.href = "javascript:void(0);";
+	
+	// icon
+	icon = d.createElement("img");
+	icon.src = "assets/images/heart_icon.png";
+	
+	// iframediv
+	iframediv = d.createElement("div");
+	iframediv.setAttribute("id", "iframediv");
+	iframediv.setAttribute("style", "display: none;");
+	iframediv.hide = false;
+	
+	// place icon into container
+	link.appendChild(icon);
+	container.appendChild(link);
+	container.appendChild(iframediv);
+	
+	// get product meta
+	var productImage = encodeURI(getMeta("name", "fb-product-image", "content")),
+		productDescription = encodeURI(getMeta("name", "fb-product-description", "content")),
+		productPrice = encodeURI(getMeta("name", "fb-product-price", "content")),
+		productName = encodeURI(getMeta("name", "fb-product-name", "content")),
+		productURL = window.location;
+		
+	// link click event handling
+	link.onclick = function (event) {	
+		var cPositionX = event.pageX,
+			cPositionY = event.pageY,
+			screenX = event.screenX,
+			screenY = event.screenY;
+		
+		//console.log(cPositionX, " - ", cPositionY, " - ", screenX, " - ", screenY, event);
+		
+		// show iframediv
+		var x = cPositionX + 15,
+			y = cPositionY + 15;
+		
+		// iframe not created yet
+		if(typeof iframe == "undefined") {
+			var link = "login.html?pi=" + productImage + "&" + "pd=" + productDescription + "&" + "pp=" + productPrice + "&pn=" + productName + "&pu=" + productURL
+			iframe = d.createElement("iframe");
+			iframe.frameBorder = 0;
+			iframe.width = "300px";
+			iframe.height = "180px";
+			iframe.id = "fb-iframe";
+			iframe.setAttribute("src", link);
+			
+			// apped iframe to iframediv
+			iframediv.appendChild(iframe);
+				
+			iframediv.setAttribute("style", "display: inline-block; position: absolute; z-index: 999; border: 1px solid #CCC; padding: 5px; top: " + y + "px; left: " + x + "px;");
+			
+			// setup clickoutside event
+			d.onclick = function () {
+				if(iframediv.hide) {
+					iframediv.setAttribute("style", "display: none;");
+					iframediv.hide = false;
+				} else {
+					iframediv.hide = true;
+				}
+			};
+		//	iframe set, show up
+		} else {
+			iframediv.setAttribute("style", "display: inline-block; position: absolute; z-index: 999; border: 1px solid #CCC; padding: 5px; top: " + y + "px; left: " + x + "px;");
+		}
+	};
+
+}(document, "fb-root"));
