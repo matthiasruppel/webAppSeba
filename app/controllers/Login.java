84fa2fe3dd00442e59fad7e5d644ef0a888151c2
gorerider <wk@plan-of-whistler.de>
2013-06-17 17:33:17 +0200

    Application now working

--- app/controllers/Login.java
+++ app/controllers/Login.java
@@ -11,120 +11,118 @@ import play.mvc.Http.Cookie;
 import play.mvc.results.Result;
 
 public class Login extends Controller {
-	
-	static User user;
-	
-	@Before
+
+    static User user;
+
+    @Before
     static void addUser() {
         User user = connected();
-        if(user != null) {
+        if (user != null) {
             renderArgs.put("user", user);
         }
     }
-	
-	static User connected() {
-        if(renderArgs.get("user") != null) {
+
+    static User connected() {
+        if (renderArgs.get("user") != null) {
             return renderArgs.get("user", User.class);
         }
         String mail = session.get("user");
-        if(mail != null) {
+        if (mail != null) {
             return User.find("mail", mail).first();
-        } 
+        }
         return null;
     }
 
-	public static void index() {
-        if(connected() != null) {
+    public static void index() {
+        if (connected() != null) {
 
-        	index();
-  
+            index();
+
         }
-        
-        if(loggedIn()){
-        	
+
+        if (loggedIn()) {
         }
-        
+
         render();
     }
-	
-	public static void register() {
+
+    public static void register() {
         render();
     }
-	
-	 public static void saveUser(@Valid User user, String verifyPassword) {
-	        validation.required(verifyPassword);
-	        validation.equals(verifyPassword, user.password).message("Your password doesn't match");
-	        if(validation.hasErrors()) {
-	            render("@register", user, verifyPassword);
-	        }
-	        user.create();
-	        session.put("user", user.mail);
-            response.setCookie("FriendsBoutiqueCookie", user.mail, "30d");
-	        flash.success("Welcome, " + user.firstName);
-	       
-	        Account.index();
-	        
-	    }
-	    
-	 	public static boolean loggedIn(){
-	 		if (Http.Request.current().cookies.get("FriendsBoutiqueCookie") != null){
-	 			String mail = Http.Request.current().cookies.get("FriendsBoutiqueCookie").value;
-	 			user = User.find("byMail", mail).first();
-	 	    	List<User> users = User.findAll();
-	 	    	if (users.contains(user)){
-	 	    		session.put("user", mail);
-		 			flash.success("Welcome, " + user.firstName);
-		 			Account.index(); 
-		 			return true;
-	 	    	}
-	 	    	else return false;
-	 		}
-	 		return false;
-	 	}
 
-	    public static void login(String mail, String password) {
-	    	
-	    	
-	    	User user = User.find("byMailAndPassword",mail, password).first();
-	        if(user != null) {
-	            session.put("user", user.mail);
-	            response.setCookie("FriendsBoutiqueCookie", mail, "30d");
-	            flash.success("Welcome, " + user.firstName);
+    public static void saveUser(@Valid User user, String verifyPassword) {
+        validation.required(verifyPassword);
+        validation.equals(verifyPassword, user.password).message("Your password doesn't match");
+        if (validation.hasErrors()) {
+            render("@register", user, verifyPassword);
+        }
+        user.create();
+        session.put("user", user.mail);
+        response.setCookie("FriendsBoutiqueCookie", user.mail, "30d");
+        flash.success("Welcome, " + user.firstName);
 
-	            
-	        Account.index();         
-	         
-	        }
-	    	
-	        // Oops
-	        flash.put("mail", mail);
-	        flash.error("Login failed");
-	        index();
-	    }
-	    
-	    
-	    public static void logout() {
-	        session.clear();
-	        response.removeCookie("FriendsBoutiqueCookie");
-	        index();
-	    }
-	    
-	    public static void settings(){
-	    	renderTemplate("Login/settings.html");	    	
-	    }
-	    
-	    public static void saveSettings(String password, String verifyPassword) {
-	        User connected = connected();
-	        connected.password = password;
-	        validation.valid(connected);
-	        validation.required(verifyPassword);
-	        validation.equals(verifyPassword, password).message("Your password doesn't match");
-	        if(validation.hasErrors()) {
-	            render("@settings", connected, verifyPassword);
-	        }
-	        connected.save();
-	        flash.success("Password was updated");
-	        Account.index();
-	    }
-	    
+        Account.index();
+
+    }
+
+    public static boolean loggedIn() {
+        if (Http.Request.current().cookies.get("FriendsBoutiqueCookie") != null) {
+            String mail = Http.Request.current().cookies.get("FriendsBoutiqueCookie").value;
+            user = User.find("byMail", mail).first();
+            List<User> users = User.findAll();
+            if (users.contains(user)) {
+                session.put("user", mail);
+                flash.success("Welcome, " + user.firstName);
+                Account.index();
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public static void login(String mail, String password) {
+
+
+        User user = User.find("byMailAndPassword", mail, password).first();
+        if (user != null) {
+            session.put("user", user.mail);
+            response.setCookie("FriendsBoutiqueCookie", mail, "30d");
+            flash.success("Welcome, " + user.firstName);
+
+
+            Account.index();
+
+        }
+
+        // Oops
+        flash.put("mail", mail);
+        flash.error("Login failed");
+        index();
+    }
+
+    public static void logout() {
+        session.clear();
+        response.removeCookie("FriendsBoutiqueCookie");
+        index();
+    }
+
+    public static void settings() {
+        renderTemplate("Login/settings.html");
+    }
+
+    public static void saveSettings(String password, String verifyPassword) {
+        User connected = connected();
+        connected.password = password;
+        validation.valid(connected);
+        validation.required(verifyPassword);
+        validation.equals(verifyPassword, password).message("Your password doesn't match");
+        if (validation.hasErrors()) {
+            render("@settings", connected, verifyPassword);
+        }
+        connected.save();
+        flash.success("Password was updated");
+        Account.index();
+    }
 }
