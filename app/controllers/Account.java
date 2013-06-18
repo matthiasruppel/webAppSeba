7d527234d911c9e239be887e03fa09856a340417
Simon Ecker <public@ecker.co.uk>
2013-06-18 10:03:56 +0200

    only other users are shown

--- app/controllers/Account.java
+++ app/controllers/Account.java
@@ -10,9 +10,16 @@ import models.*;
 public class Account extends Controller {
 
     public static void index() {
+    		String currentUser = session.get("user");
 	    	List<User> users = User.findAll();
+	    	for(int i = 0; i < users.size(); i++){
+	    	 if(users.get(i).mail.equals(currentUser)){
+	    		 users.remove(i);
+	    	 }
+	    	}
+	    
 	    	//List<Items> items = Items.findAll();
-	    	String currentUser = session.get("user");
+	    	
         render(users, currentUser);
     }
     
