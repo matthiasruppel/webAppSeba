5dc2661152adc1e0688e2efcffb247f75b919258
Simon Ecker <public@ecker.co.uk>
2013-06-18 21:11:52 +0200

    added item functionality

--- app/controllers/Account.java
+++ app/controllers/Account.java
@@ -5,22 +5,41 @@ import play.mvc.*;
 
 import java.util.*;
 
+import com.sun.xml.internal.ws.developer.UsesJAXBContext;
+
 import models.*;
 
 public class Account extends Controller {
+	
+	
 
     public static void index() {
     		String currentUser = session.get("user");
+	    	List<Item>items = Item.findAll();
 	    	List<User> users = User.findAll();
 	    	for(int i = 0; i < users.size(); i++){
 	    	 if(users.get(i).mail.equals(currentUser)){
 	    		 users.remove(i);
 	    	 }
 	    	}
-	    
-	    	//List<Items> items = Items.findAll();
 	    	
-        render(users, currentUser);
+	    	
+	    	for(int i = 0; i < items.size(); i++){
+		    		if(!items.get(i).userlist.equals(currentUser)){
+		    		items.remove(i); 		    		 
+		    		}
+	    	}
+	    	
+	    	if (users.isEmpty()){
+	            flash.success("Sorry, no other users");
+	    	}
+	    	
+	    	if (items.isEmpty()){
+	            flash.success("Sorry, no items");
+	    	}
+	    
+	    	
+        render(users, currentUser, items);
     }
     
    
---
+++ app/models/Item.java
@@ -0,0 +1,54 @@
+package models;
+
+import play.*;
+import play.data.validation.MaxSize;
+import play.data.validation.MinSize;
+import play.data.validation.Required;
+import play.db.jpa.*;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+public class Item extends Model {
+
+	public long articleNr;
+	
+	@Required
+	public String title;
+	
+	public String subtitle;
+	
+	public String description;
+	
+	@Required
+	public String Category;
+	
+	@Required
+	public String brand;
+	
+	@Required
+	public String shop;
+	
+	public double price;
+	
+	
+    public String userlist;
+	
+	public Item(long articleNr, String title, String subtitle, String category,
+			String brand, String shop, double price, String userlist) {
+		
+		this.articleNr = articleNr;
+		this.title = title;
+		this.subtitle = subtitle;
+		Category = category;
+		this.brand = brand;
+		this.shop = shop;
+		this.price = price;
+		this.userlist = userlist;
+		
+	}
+	
+	
+	
+}
