2751b73c24c02a0aa21b8b1e4acc7fbeb8ce3732
Simon Ecker <public@ecker.co.uk>
2013-06-20 14:24:15 +0200

    adding items works

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -18,11 +18,14 @@ public class Application extends Controller {
 //    	flash.success("Please sign-in first.");
 //    	render("NotImplemented/index.html");
     }
-
+    
+    public static void addItemView(){
+    	renderTemplate("Application/addItem.html");
+    }
+    	
     public static void addItem(long articleNr, String title, String subtitle, String category,
             String brand, String shop, double price, String userlist) {
-
-        render("@addItem", articleNr, title, subtitle, category, brand, shop, price);
+        //render("@addItem", articleNr, title, subtitle, category, brand, shop, price);
         String currentUser = session.get("user");
         List<Item> items = Item.findAll();
         List<User> users = User.findAll();
@@ -34,8 +37,8 @@ public class Application extends Controller {
                 theUser = users.get(i);
             }
         }
-        new Item(articleNr, title, subtitle, category, brand, shop, price, theUser.mail);
-        System.out.println(userlist);
+        new Item(articleNr, title, subtitle, category, brand, shop, price, currentUser);
+        //System.out.println(userlist);
         Account.index();
     }
 
--- app/models/Item.java
+++ app/models/Item.java
@@ -46,6 +46,7 @@ public class Item extends Model {
 		this.shop = shop;
 		this.price = price;
 		this.userlist = userlist;
+		create();
 		
 	}
 	
