a55ee8dce698def0b076c843777a314adeb10c37
Simon Ecker <public@ecker.co.uk>
2013-06-20 17:19:20 +0200

    visit feature added

--- app/controllers/Visit.java
+++ app/controllers/Visit.java
@@ -12,35 +12,47 @@ import models.*;
 public class Visit extends Controller {
 
     public static void index(String mail) {
-
-        if (Login.connected() == null) {
-
+    	if (Login.loggedIn() == false) {
             flash.success("Please sign-in first.");
             Login.index();
         }
-        String visitUserFirst = "";
-        String visitUserLast = "";
-
+    	
+    	
         String currentUser = session.get("user");
-
         List<Item> items = Item.findAll();
+        List<Item> userItem = new ArrayList();
         List<User> users = User.findAll();
+        int j = 0;
+        User theUser = null;
+        String visitedUserFirst = "";
+        String visitedUserLast = "";
+        for (int i = 0; i < users.size(); i++) {
+            // 	System.out.println(users.get(i));
+            if (users.get(i).mail.equals(currentUser)) {
+                theUser = users.get(i);
+            }
+            if (users.get(i).mail.equals(mail)){
+            	visitedUserFirst = users.get(i).firstName;
+            	visitedUserLast = users.get(i).lastName;
+            }
+        }
+        
+        
+        
+        //System.out.println("the user: "+theUser);
         for (int i = 0; i < users.size(); i++) {
             if (users.get(i).mail.equals(currentUser)) {
                 users.remove(i);
-            }
-            if (users.get(i).mail.equals(mail)) {
-                visitUserFirst = users.get(i).firstName;
-                visitUserLast = users.get(i).lastName;
-
             }
         }
 
 
         for (int i = 0; i < items.size(); i++) {
-            if (!items.get(i).userlist.equals(mail)) {
-                items.remove(i);
+            if (items.get(i).userlist.equals(mail)) {
+            userItem.add(j, items.get(i));
+            j++;
             }
+            
         }
 
         if (users.isEmpty()) {
@@ -52,6 +64,8 @@ public class Visit extends Controller {
         }
 
 
-        render(users, currentUser, items, visitUserFirst, visitUserLast);
+        render(users, currentUser, items, theUser, userItem, mail, visitedUserFirst, visitedUserLast);
+    
+
     }
 }
